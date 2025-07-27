ed0bc23126f0edbc84de1c32e0a7ad3e0c486b41
an-144 <153922167+an-144@users.noreply.github.com>
2025-07-26 20:04:45 -0400

    Update ManagementLogin.jsx

--- app/(tabs)/ManagementLogin.jsx
+++ app/(tabs)/ManagementLogin.jsx
@@ -32,7 +32,7 @@ const ManagementLogin = () => {
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.headerContainer}>
         <Image source={require('../../assets/images/logo.png')} style={styles.logo} />
-        <Text style={styles.headerText}>SCDC SMART</Text>
+        <Text style={styles.headerText}>RunningHour</Text>
       </View>
 
       <Text style={styles.title}>Management Login</Text>
